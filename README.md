21c9397bb4d1c27f7db0d70f0c8dd147bb501ccc
windiewhale <63591450+windiewhale@users.noreply.github.com>
2022-01-19 20:00:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Pretty Simple Web server written in Node.js
+
+You can use this repository as a template for your repository or copy the files into your repository manually
+
+1. Open a new terminal (eg. in VS Code when your repository's directory opened)
+2. Run with the following command: `node backend/server.js` (Make sure that you are in the right directory.)
+3. If you don't need it stop it with `Control + C` keycommand.
+
+You can write your frontend code (`HTML, CSS, JS`) into the `frontend` directory.
---
+++ backend/server.js
@@ -0,0 +1,69 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+
+	const errorHTML = `
+		
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="preconnect" href="https://fonts.googleapis.com"> 
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+		<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@800&display=swap" rel="stylesheet">
+		<style>
+			body{
+				padding: 0; margin: 0;
+				font-family: 'Montserrat', sans-serif;
+				font-weight: 800;
+				background-color: #4343F9;
+				color: #fff;
+			}
+			#root{
+				width: 100%;
+				height: 100vh;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				font-size: 21px;
+			}
+		</style>
+		<title>Not here</title>
+	</head>
+	<body>
+		<div id="root">Rise your gaze to the sky<br/>than a bit back to the URL bar<br/>and check that link again</div>
+	</body>
+	</html>
+	
+	`;
+    
+	let filePath = path.resolve(__dirname + '/../frontend' + req.url);
+    
+	fs.access(filePath, fs.constants.R_OK, (err) => {
+	if(err){
+		res.statusCode = 404;
+		res.end(errorHTML);
+	}else{
+		if(fs.statSync(filePath).isDirectory()) {
+			filePath += '/index.html';
+		}
+		fs.readFile(filePath, (err, data) => {
+			if(err) {
+				res.statusCode = 500;
+				res.end(errorHTML);
+			} else {
+				res.end(data);
+			}
+		});
+	}
+	});
+});
+
+server.listen(9000, "127.0.0.1", () => {
+    const addr = server.address();
+		console.log(`http://${addr.address}:${addr.port}`);
+});
